53285ab5b714fe2c2d0a2f91343ca7001bce9e46
8_fernando-jpg <fergc0110.08@gmail.com>
2026-01-09 10:53:57 +0100

    JavaScript

--- prueba.js
+++ prueba.js
@@ -10,6 +10,35 @@ debugger
 if (esViernes == true) {
 console.log('es viernes, nos vamos de fiesta');
 }
-var nombreUsuario = prompt("intruduzca su nombre")
-console.log("El nombre del usuario:", nombreUsuario);
-document.write("<h1>" + nombreUsuario +"</h1>")
+console.log('el nombre del usuario es: ' + nombreUsuario);
+var nombreUsuario = document.getElementById('nombre').value;
+document.write("<h1>"+ nombreUsuario + "</h1>");
+function comprobarFormulario() {
+   var pass1 = document.getElementById('pass1').value;
+   var pass2 = document.getElementById('pass2').value;
+   return pass1 === pass2;
+}
+
+
+function enviarfolmulario(){
+   console.log('enviando formulario...');
+    var dic = document.getElementById('mensajes');
+
+
+
+
+   if (comprobarFormulario) {
+       var nombre = document.getElementById('nombre').value;
+       div.innerHTML = nombre + "Password Ok";
+       div.style.backgroundColor = "green";      
+       //document.write("<h1>" + nombre + "</h1>");
+       //document.write("<p>Password Ok</p>");
+   } else {
+       div.innerHTML = "Password ko";
+       div.style.backgroundColor = "red";
+}
+}
+function cambiarColor(color){
+    var div = document.getElementById("mensajes")
+    div.style.backgroundColor = color;
+}
